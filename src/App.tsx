530b8caa8e52b1033b64bf6a956ac3ecf381dac8
BensonFeng <bensonfeng@outlook.com>
2021-12-15 22:26:52 -0500

    chrildren prop type

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import "./App.css";
+import { Heading } from "./components/Heading";
+import { Status } from "./components/Status";
+import { Oscar } from "./components/Oscar";
 import { Greet } from "./components/Greet";
-import { Person } from "./components/Person";
-import { PersonList } from "./components/PersonList";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
-
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne",
-    },
-    {
-      first: "Clark",
-      last: "Kent",
-    },
-    {
-      first: "Princess",
-      last: "Diana",
-    },
-  ];
   return (
     <div className="App">
-      <Greet name="Ben" messageCount={20} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
+      <Status status="loading" />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to...</Heading>
+      </Oscar>
+      <Greet name="Ben" messageCount={10} isLoggedIn={true} />
     </div>
   );
 }
