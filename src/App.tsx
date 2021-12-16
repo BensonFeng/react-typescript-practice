9c056bef7ff267a9e7e997fdca6e223a893985d8
BensonFeng <bensonfeng@outlook.com>
2021-12-16 15:06:38 -0500

    props styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import "./App.css";
-import { Button } from "./components/Button";
-import { Input } from "./components/Input";
+import { Container } from "./components/Container";
 
 function App() {
   return (
     <div className="App">
-      <Button
-        handleClick={(event, id) => {
-          console.log("Button clicked", event, id);
+      <Container
+        styles={{
+          border: "1px solid black",
+          padding: "1rem",
+          marginTop: "1rem",
         }}
       />
-      <Input
-        value=""
-        handleChange={(event) => console.log(event.target.value)}
-      ></Input>
     </div>
   );
 }
