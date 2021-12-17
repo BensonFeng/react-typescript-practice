5fed1d8aade947be272b885b12d64c5ba34e01ee
BensonFeng <bensonfeng@outlook.com>
2021-12-17 16:29:19 -0500

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import "./App.css";
-import { UserContextProvider } from "./components/context/UserContext";
-import { User } from "./components/context/User";
+
 function App() {
-  return (
-    <div className="App">
-      <UserContextProvider>
-        <User />
-      </UserContextProvider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 export default App;
