a8e388a24e01d11aa0f9f7c2ee37f09e3b522842
BensonFeng <bensonfeng@outlook.com>
2021-12-17 18:13:37 -0500

    pass component as props type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import "./App.css";
+import { Private } from "./components/auth/Private";
+import { Profile } from "./components/auth/Profile";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Private isLoggedIn={true} component={Profile} />
+    </div>
+  );
 }
 export default App;
